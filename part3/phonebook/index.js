83978976fcfa9716fb2bb659259ddb4ac78ac44c
Algebrazebra <Algebrazebra@users.noreply.github.com>
2021-10-17 13:13:39 +0200

    Add exercises 3.13-3.14

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 var logger = require('morgan')
 
@@ -7,52 +8,34 @@ app.use(logger(':method :url :status :res[content-length] - :response-time ms :b
 app.use(express.static('build'))
 app.use(express.json())
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
-const generateId = () => {
-    return Math.floor(Math.random() * 10_000_000);
-}
+const Person = require('./models/person')
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+  Person
+    .find({})
+    .then(persons => {
+      response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(note => note.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Person
+    .findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else {
+        response.status(404).end()
+      }
+    })
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
+  Person
+    .findByIdAndDelete(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
 })
 
 app.post('/api/persons/', (request, response) => {
@@ -61,28 +44,22 @@ app.post('/api/persons/', (request, response) => {
     return response.status(400).json({
         error: 'content missing'
     })
-  } else if (persons.map(p => p.name).includes(body.name)) {
-    return response.status(400).json({
-      error: `Person with name ${body.name} already exists.`
-    })
   }
-  const person = {
-      id: generateId(),
+  const person = new Person({
       name: body.name,
       number: body.number
-  }
-  persons = persons.concat(person)
-  response.json(person)
+  })
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
 
 app.get('/info', (request, response) => {
-    const info = {
-        "people": persons.length,
-        "date": Date.now()
-    }
-    const htmlString = `<div>Phonebook has info for ${persons.ength}</div><div>${new Date().toLocaleString()}</div>`
+  Person.countDocuments({}, (error, count) => {
+    const htmlString = `<div>Phonebook has info for ${count} persons.</div><div>${new Date().toLocaleString()}</div>`
     response.set('Content-Type', 'text/html')
     response.send(Buffer.from(htmlString))
+  })
 })
 
 
