4e34a9cbaeee833eb31a5fb1d3c94d68ff77ac09
Algebrazebra <Algebrazebra@users.noreply.github.com>
2021-12-28 20:03:11 +0100

    fixup! Add exercise 5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react'
+import blogService from '../services/blogs'
 
 const Blog = ({blog}) => {
 
-  const [showDetails, toggleDetails] = useState(false)
+  const [showDetails, setShowDetails] = useState(false)
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails)
+  }
 
   const blogStyle = {
     paddingTop: 10,
@@ -12,12 +17,24 @@ const Blog = ({blog}) => {
     marginBottom: 5
   }
 
+  const handleLike = async (blog) => {
+    var updatedBlog = {
+      id: blog.id,
+      title: blog.title,
+      url: blog.url,
+      likes: blog.likes + 1
+    }
+    if (typeof(blog.user) !== 'undefined') {
+      updatedBlog['user'] = blog.user.id
+    }
+    await blogService.update(updatedBlog)
+  }
+
   const BlogDetail = () => {
-    console.log(blog)
     return (
       <div>
         {blog.url}<br />
-        likes: {blog.likes} <button>like</button> <br />
+        likes: {blog.likes} <button onClick={() => handleLike(blog)}>like</button> <br />
         {typeof(blog.user) !== 'undefined' && blog.user.name}
       </div>
     )
