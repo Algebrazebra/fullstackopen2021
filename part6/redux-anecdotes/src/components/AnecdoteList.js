2f371b7dcf549249d73890769dd8e0db36aaa166
Algebrazebra <Algebrazebra@users.noreply.github.com>
2022-01-15 17:00:58 +0100

    Add exercise 6.5-6.8

---
+++ part6/redux-anecdotes/src/components/AnecdoteList.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { voteAnecdote } from '../reducers/anecdoteReducer'
+
+
+const AnecdoteList = () => {
+
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(voteAnecdote(id))
+  }
+
+  const compareAnecdotes = (a, b) => {
+    return a.votes > b.votes ? -1 : 1
+  }
+
+  return (
+    <div>
+      {anecdotes
+        .sort(compareAnecdotes)
+        .map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
